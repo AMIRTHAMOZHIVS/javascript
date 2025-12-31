d13baed7b32a3eba5ee480cf1a8d573cb571cbfe
AMIRTHAMOZHIVS <amirthashan06@gmail.com>
2025-12-31 10:10:07 +0530

    Relationaloperators

---
+++ relationaloperators.js
@@ -0,0 +1,18 @@
+let age1 = 25
+let age2 = "25"
+let score = 85
+if(age1==age2){
+    console.log("true")
+}
+else{
+    console.log("false")
+}
+if(score>80){
+    console.log("true")
+}
+else{
+     console.log("false")
+}
+if(age1===age2){
+     console.log("True")
+}

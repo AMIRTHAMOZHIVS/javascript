47e13ebdac2b89e4ca304aad31efab70956a2e9e
AMIRTHAMOZHIVS <amirthashan06@gmail.com>
2025-12-31 10:09:28 +0530

    Arithmeticoperators

---
+++ arithmeticoperators.js
@@ -0,0 +1,10 @@
+let len= 15;
+let wid = 4;
+let area = len * wid;
+console.log("Area : " +area);
+let perimeter = 2*(len+wid)
+console.log(perimeter);
+let div = len/3;
+console.log(div);
+let rem = wid%2;
+console.log(rem);

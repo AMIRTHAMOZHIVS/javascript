1dba29ed39534ff9f7099f8fc9ac7897484e1ac6
AMIRTHAMOZHIVS <amirthashan06@gmail.com>
2025-12-31 10:24:58 +0530

    Bitwise Operators

---
+++ bitwiseoperators.js
@@ -0,0 +1,13 @@
+let a =12
+let b = 7
+if(a>0&&a<255){
+    console.log("VALUES : ")
+    console.log(a&b)
+    console.log(a|b)
+    console.log(a^b)
+    //console.log(a ~ b)
+    console.log(a<<2)
+    console.log(a>>1)
+    console.log(b<<2)
+    console.log(b>>1)
+}

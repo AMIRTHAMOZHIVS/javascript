eca722965f4794038efd92b9aa35756e7b19d9d0
AMIRTHAMOZHIVS <amirthashan06@gmail.com>
2026-01-01 21:28:56 +0530

    Understanding function calls in Js

---
+++ functions.js
@@ -0,0 +1,16 @@
+function samsung(){
+    console.log("This is samsung")
+}
+function oppo(){
+    console.log("This is Oppo")
+}
+function  vivo(){
+    console.log("This is vivo")
+}
+function realme(){
+    console.log("This is realme")
+}
+realme()
+oppo()
+vivo()
+samsung()
